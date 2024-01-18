4276eca519f3d606a141b457b592f83f58a8f0a0
RWS56 <ruben.steen@skola.taby.se>
2024-01-18 13:49:48 +0100

    Fixed redrawing issue

--- game.js
+++ game.js
@@ -19,20 +19,17 @@ class Game{
     run(){
         this.update();
         //remove testcode below this
-        this.tilemap["5;7"] = {"type": "testTile"};
+        //game.tilemap.tilemap[`${x};${y}`] = {"type":"testTile", "variant": 0, "pos": [x, y]};
     }
 
     //main game loop//main game loop//main game loop//main game loop
     update(){ //main game loop//main game loop//main game loop
         requestAnimationFrame(this.update.bind(this));
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.beginPath();
         this.tilemap.draw(this.offset);
     }
 }
 
 let game = new Game(document.getElementById("canvas"));
 game.run();
-for(let i = 0; i < 100; i++){
-    let x = Math.floor(Math.random() * 50);
-    let y = Math.floor(Math.random() * 50);
-    game.tilemap.tilemap[`${x};${y}`] = {"type":"testTile", "variant": 0, "pos": [x, y]};
-}
