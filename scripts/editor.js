ee529137663c785819fb0eaa0a51afd569d978b8
RWS56 <ruben.steen@skola.taby.se>
2024-01-19 17:09:58 +0100

    Tilemap and editor improvements

--- scripts/editor.js
+++ scripts/editor.js
@@ -4,14 +4,19 @@ class Game{
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
+        
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
-        this.canvas.backgroundColor = "red";
         this.renderScale = 4;
         this.ctx.scale(this.renderScale, this.renderScale)
+
         this.movement = [false, false, false, false];
         this.scrollOffset = [0, 0];
         this.mouseDown = false;
+        this.rightClick = false;
+
+        this.keys = {};
+        this.mousePos = [0, 0]
 
         this.assets = { 
             "grass" : loadImages("tiles/grass", 9),
@@ -21,13 +26,44 @@ class Game{
         this.tileType = 0;
         this.tileVariant = 0;
 
-        this.tilemap = new Tilemap(this, this.ctx, this.canvas, 16);
+        this.tilemap = new Tilemap(this, this.ctx, this.canvas, 16, this.renderScale);
     }
 
     run(){
+        document.addEventListener('keydown', (event) => {
+            this.keys[event.key] = true;
+        });
+        
+        document.addEventListener('keyup', (event) => {
+            this.keys[event.key] = false;
+        });
+        
+        document.addEventListener('mousemove', (event) => {
+            this.mousePos[0] = Math.floor(event.clientX / this.renderScale);
+            this.mousePos[1] = Math.floor(event.clientY / this.renderScale);
+        });
+        
+        document.addEventListener('mousedown', (event) => {
+            if (event.button === 0){
+                this.mouseDown = true;  
+            }
+            else if (event.button === 2){
+                this.rightClick = true;  
+            }
+        });
+        
+        document.addEventListener('mouseup', (event) => {
+            this.mouseDown = false;
+            this.rightClick = false;
+        });
+        
+        window.addEventListener('resize', (event) => {
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
+            this.ctx.scale(this.renderScale, this.renderScale);
+        });
+        
         this.update();
-        //remove testcode below this
-        //game.tilemap.tilemap[`${x};${y}`] = {"type":"testTile", "variant": 0, "pos": [x, y]};
     }
 
     //main game loop
@@ -47,8 +83,8 @@ class Game{
         this.tilemap.draw(renderScroll);
 
         let selectedTileImage = this.assets[this.tileArray[this.tileType]][this.tileVariant];
-        let mouseGridPos = [Math.floor((mousePos[0] + this.scrollOffset[0]) / this.tilemap.tileSize), Math.floor((mousePos[1] + this.scrollOffset[1]) / this.tilemap.tileSize)]
-        this.ctx.globalAlpha = 0.4;
+        let mouseGridPos = [Math.floor((this.mousePos[0] + this.scrollOffset[0]) / this.tilemap.tileSize), Math.floor((this.mousePos[1] + this.scrollOffset[1]) / this.tilemap.tileSize)]
+        this.ctx.globalAlpha = 0.45;
         this.ctx.drawImage(selectedTileImage, mouseGridPos[0] * this.tilemap.tileSize - this.scrollOffset[0], mouseGridPos[1] * this.tilemap.tileSize - this.scrollOffset[1], this.tilemap.tileSize, this.tilemap.tileSize);
         this.ctx.globalAlpha = 1;
 
@@ -60,51 +96,26 @@ class Game{
         this.movement[1] = false;
         this.movement[2] = false;
         this.movement[3] = false;
-        if(keys["a"]){
+        if(this.keys["a"]){
             this.movement[0] = true;
         }
-        if(keys["d"]){
+        if(this.keys["d"]){
             this.movement[1] = true
         }
-        if(keys["w"]){
+        if(this.keys["w"]){
             this.movement[2] = true;
         }
-        if(keys["s"]){
+        if(this.keys["s"]){
             this.movement[3] = true
+        }
+        if(this.keys["t"]){
+            this.tilemap.autoTile();
+        }
+        if(this.keys["o"]){
+            this.tilemap.save();    
         }
     }
 }
-
-//fix this later vvvvvvvv
-let keys = {};
-let mousePos = [0, 0]
-//^^^^^^^^^^^^^^^^^^
-document.addEventListener('keydown', function(event) {
-    keys[event.key] = true;
-});
-
-document.addEventListener('keyup', function(event) {
-    keys[event.key] = false;
-});
-
-document.addEventListener('mousemove', function(event) {
-    mousePos[0] = Math.floor(event.clientX / game.renderScale);
-    mousePos[1] = Math.floor(event.clientY / game.renderScale);
-});
-
-document.addEventListener('mousedown', function(event) {
-    game.mouseDown = true;
-});
-
-document.addEventListener('mouseup', function(event) {
-    game.mouseDown = false;
-});
-
-window.addEventListener('resize', function(event) {
-    game.canvas.width = window.innerWidth;
-    game.canvas.height = window.innerHeight;
-    game.ctx.scale(game.renderScale, game.renderScale);
-});
 
 let game = new Game(document.getElementById("canvas"));
 game.run();
--- scripts/tilemap.js
+++ scripts/tilemap.js
@@ -1,12 +1,26 @@
 const neighborOffset = [[-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [0, 0], [-1, 1], [0, 1], [1, 1]];
 const physicsTiles = ["grass"];
+const autoTiledTiles = ["grass"];
+
+const autoTileMapRuleset = {
+    "0,1,1,0": 0,
+    "-1,0,0,1,1,0": 1,
+    "-1,0,0,1": 2,
+    "0,-1,0,1,1,0": 3,
+    "-1,0,0,-1,0,1,1,0": 4,
+    "-1,0,0,-1,0,1": 5,
+    "0,-1,1,0": 6,
+    "-1,0,0,-1,1,0": 7,
+    "-1,0,0,-1": 8
+};
 
 class Tilemap{
-    constructor(game, ctx, canvas, tileSize = 16){
+    constructor(game, ctx, canvas, tileSize = 16, renderScale){
         this.game = game;
         this.ctx = ctx;
         this.canvas = canvas
         this.tileSize = tileSize;
+        this.renderScale = renderScale;
         this.tilemap = {};
         this.offgridTiles = {};
     }
@@ -28,12 +42,40 @@ class Tilemap{
         return tiles;
     }
 
-    save(){
-        let data = JSON.stringify({"tilemap": this.tilemap, "tileSize": this.tileSize, "offgridTiles": this.offgridTiles})
+    autoTile(){
+        for(let location in this.tilemap){
+            let tile = this.tilemap[location];
+            let neighbors = [];
+            for(let offset of [[1, 0], [0, 1], [-1, 0], [0, -1]]){
+                let checkLocation = `${tile["pos"][0] + offset[0]};${tile["pos"][1] + offset[1]}`
+                if(checkLocation in this.tilemap){
+                    if(this.tilemap[checkLocation]["type"] == tile["type"]){
+                        neighbors.push(offset);
+                    }
+                }
+            }
+            neighbors = neighbors.sort().toString();
+            if(autoTiledTiles.includes(tile["type"]) && neighbors in autoTileMapRuleset){
+                tile["variant"] = autoTileMapRuleset[neighbors];
+            }
+        }
+    }
+
+    save(){ //dåligt sätt
+        let data = JSON.stringify({"tilemap": this.tilemap, "tileSize": this.tileSize, "offgridTiles": this.offgridTiles});
+        let element = document.createElement("a");
+        element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(data));
+        element.setAttribute("download", "map.json");
+
+        element.style.display = "none";
+        document.body.appendChild(element);
+        element.click();
+
+        document.body.removeChild(element);
     }
 
     load(){
-        
+        let data;
     }
 
     getPhysicsRectAround(position){ //använd for(let tile in x) etc ungefär som pythons for tile in tiles etc
@@ -48,8 +90,8 @@ class Tilemap{
     }
     
     draw(offset = [0, 0]){
-        for(let x = Math.floor(offset[0] / this.tileSize) - 1; x < Math.floor((offset[0] + window.innerWidth) / this.tileSize) + 1; x++){
-            for(let y = Math.floor(offset[1] / this.tileSize); y < Math.floor((offset[1] + window.innerHeight) / this.tileSize) + 1; y++){
+        for(let x = Math.floor(offset[0] / this.tileSize) - 1; x < Math.floor((offset[0] + window.innerWidth / this.renderScale) / this.tileSize) + 1; x++){
+            for(let y = Math.floor(offset[1] / this.tileSize) - 1; y < Math.floor((offset[1] + window.innerHeight / this.renderScale) / this.tileSize) + 1; y++){
                 let checkLocation = `${x};${y}`;
                 if(checkLocation in this.tilemap){
                     let tile = this.tilemap[checkLocation];
